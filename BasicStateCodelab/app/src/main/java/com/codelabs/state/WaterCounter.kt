1d893202698fd860d9f2aa57cb1b911ff3c7b55a
Woogear Kwon <techkwon1717@hanmail.net>
2022-11-30 13:23:23 +0900

    Restore state in Compose

--- BasicStateCodelab/app/src/main/java/com/codelabs/state/WaterCounter.kt
+++ BasicStateCodelab/app/src/main/java/com/codelabs/state/WaterCounter.kt
@@ -1,11 +1,11 @@
 package com.codelabs.state
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -14,7 +14,7 @@ import com.codelabs.state.ui.theme.BasicStateCodelabTheme
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
     Column(modifier = modifier.padding(16.dp)) {
-        var count by remember { mutableStateOf(0) }
+        var count by rememberSaveable { mutableStateOf(0) }
         if (count > 0) {
             Text("You've had $count glasses.")
         }
