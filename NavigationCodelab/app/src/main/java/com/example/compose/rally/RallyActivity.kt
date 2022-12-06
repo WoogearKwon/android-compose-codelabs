52e7c11c845ba7957fa9a39d2357a95fc6c2e26a
Woogear Kwon <techkwon1717@hanmail.net>
2022-12-06 11:35:14 +0900

    Integrate RallyTabRow with navigation

--- NavigationCodelab/app/src/main/java/com/example/compose/rally/RallyActivity.kt
+++ NavigationCodelab/app/src/main/java/com/example/compose/rally/RallyActivity.kt
@@ -21,10 +21,14 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Scaffold
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.example.compose.rally.ui.components.RallyTabRow
 import com.example.compose.rally.ui.theme.RallyTheme
@@ -45,14 +49,21 @@ class RallyActivity : ComponentActivity() {
 @Composable
 fun RallyApp() {
     RallyTheme {
-        var currentScreen: RallyDestination by remember { mutableStateOf(Overview) }
         val navController = rememberNavController()
+        val currentBackStack by navController.currentBackStackEntryAsState()
+        // Fetch your currentDestination
+        val currentDestination = currentBackStack?.destination
+
+        // Change the variable to this and use Overview as a backup screen if this returns null
+        val currentScreen = rallyTabRowScreens.find { it.route == currentDestination?.route } ?: Overview
 
         Scaffold(
             topBar = {
                 RallyTabRow(
                     allScreens = rallyTabRowScreens,
-                    onTabSelected = { screen -> currentScreen = screen },
+                    onTabSelected = { newScreen ->
+                        navController.navigateSingleTopToo(newScreen.route)
+                    },
                     currentScreen = currentScreen
                 )
             }
@@ -75,3 +86,18 @@ fun RallyApp() {
         }
     }
 }
+
+fun NavHostController.navigateSingleTopToo(route: String) =
+    this.navigate(route) {
+        // pop up to the start destination of the graph to avoid building up a large of destinations
+        // on the back stack as you select tabs.
+        // In Rally, this would mean that pressing the back arrow from any destination would pop the
+        // entire back stack to Overview
+        popUpTo(
+            this@navigateSingleTopToo.graph.findStartDestination().id
+        ) { saveState = true}
+        launchSingleTop = true
+        // `restore = true` determines whether this navigation action should restore any state
+        // previously saved by PopUpToBuilder.saveState or the popUpToSaveState attribute.
+        restoreState = true
+    }
